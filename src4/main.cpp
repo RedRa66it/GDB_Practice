f1fa75ffde6c59d327728ffb0dd3c67148e1ca03
bsg04103 <bsg04103@gmail.com>
2025-03-08 08:55:36 +0900

    Sample Code for Practicing GDB

---
+++ src4/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "square.hpp"
+
+int main() {
+	std::cout << squareArea(6) << std::endl;
+	return 0;
+}
---
+++ src4/square.hpp
@@ -0,0 +1,8 @@
+#ifndef SQUARE_HPP_
+#define SQUARE_HPP_
+
+inline double squareArea(double perimeter) {
+		return (perimeter / 4) * (perimeter / 4);
+}
+
+#endif // SQUARE_HPP
